e441e13bfbd40a94e84ddb0d0caaa03b29ac1357
Sandro <sandro.fonseca@cern.ch>
2017-11-23 15:48:16 +0100

    included list of triggers

--- JpsiAnalyzerOpen2011/test/JPSIAnaOpenData2011_Model.py
+++ JpsiAnalyzerOpen2011/test/JPSIAnaOpenData2011_Model.py
@@ -6,12 +6,13 @@ process = cms.Process("Demo")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(20) )
 
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
-    'root://eospublic.cern.ch//eos/opendata/cms/MonteCarlo2011/Summer11LegDR/JPsiToMuMu_2MuPEtaFilter_7TeV-pythia6-evtgen-v2/AODSIM/PU_S13_START53_LV6-v1/00000/0041841F-AC33-E411-8A6E-00266CFFA038.root'       
+    #'root://eospublic.cern.ch//eos/opendata/cms/MonteCarlo2011/Summer11LegDR/JPsiToMuMu_2MuPEtaFilter_7TeV-pythia6-evtgen-v2/AODSIM/PU_S13_START53_LV6-v1/00000/0041841F-AC33-E411-8A6E-00266CFFA038.root'  
+    'root://eospublic.cern.ch//eos/opendata/cms/Run2011A/MuOnia/AOD/12Oct2013-v1/20000/0045B338-934C-E311-BEF1-0026189438B0.root'     
     )
 )
 
@@ -27,23 +28,23 @@ process.TFileService = cms.Service ('TFileService',
 )
  
 process.demo = cms.EDAnalyzer('JpsiAnalyzerOpen2011',
-		#trigger_on = cms.bool(True),
         verbose = cms.bool(False),
 	triggerflag = cms.bool(True), # False = Data and True = MC		      
 	# Trigger
 	TriggerResultsTag = cms.untracked.InputTag("TriggerResults", "", "HLT"),
-    TriggerEventTag = cms.untracked.InputTag("hltTriggerSummaryAOD", "", "HLT"),
-    PathName = cms.untracked.string("HLT_DoubleMu7_v"),
-    # RECO Labels
-    primaryVertexProducer = cms.InputTag("offlinePrimaryVertices"),
-	recoMuonsLabel = cms.InputTag("muons"),   
+        TriggerEventTag = cms.untracked.InputTag("hltTriggerSummaryAOD", "", "HLT"),
+        #PathName = cms.untracked.vstring("HLT_DoubleMu7_v"),
+        PathName = cms.untracked.vstring(["HLT_Dimuon10_Jpsi_Barrel_v1","HLT_Dimuon7_PsiPrime"]),
+        # RECO Labels
+        primaryVertexProducer = cms.InputTag("offlinePrimaryVertices"),
+        recoMuonsLabel = cms.InputTag("muons"), 
 	# RECO Configs
-    minMuPt = cms.double(2.0),# in GeV
-    maxMuEta = cms.double(2.4),
-    minMuonLeadPt = cms.double(20.0),# in GeV
-	minMuonTrailPt = cms.double(4.0), # in GeV
-	minJPsiMass = cms.double(2.95),# in GeV
-	maxJPsiMass = cms.double(3.25)# in GeV 
+        minMuPt = cms.double(2.0),# in GeV
+        maxMuEta = cms.double(2.4),
+        minMuonLeadPt = cms.double(20.0),# in GeV
+        minMuonTrailPt = cms.double(4.0), # in GeV
+        minJPsiMass = cms.double(2.95),# in GeV
+        maxJPsiMass = cms.double(3.25)# in GeV 
 )
 
 
